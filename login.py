be17f1d5c11e7e0d1f7fb5dce61bc7711b45330e
varadaakash1725 <100934002+varadaakash1725@users.noreply.github.com>
2022-03-05 13:53:45 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,15 +1,6 @@
 
-from django import Django,request
+from django import Django,redirect
 
-app=Django(__name__)
-
-@app.route('/',methods=['post'])
-def getUserId():
-    userId=request.json["userId"]
-    return userId
-def getPassword():
-    password=request.json["password"]
-    return password
 
 import mysql.connector
 
@@ -18,11 +9,20 @@ mydb=mysql.connector.connect(
     database="dbs_project"
 )
 mycursor=mydb.cursor()
+def login(request):
+    if request.method=="POST" :
+        userName=request.POST.get("userName").strip.lower()
+        password=request.POST.get("password")
+        try:
+            checkUserId = mycursor.execute("Select userId from registered_users r where r.userId=? and r.password=?",(getUserId(),getPassword))
+            if checkUserId !=0:
+                return redirect("{% '#'  %} ")
+            raise Exception()
+        except:
+            messages.add_message(request, messages.INFO,"You have supplied invalid login credentials, please try again!", "danger")
+            return redirect("{% '#'  %} ")
+    return render(request,"login.html")
 
-checkUserId = mycursor.execute("Select userId from registered_users r where r.userId=? and r.password=?",(getUserId(),getPassword))
-
-if checkUserId !=0
-    
 
 
 
