4458c6b724f5f73a1c94ec42a1b6528afd1f9a2d
varadaakash1725 <100934002+varadaakash1725@users.noreply.github.com>
2022-03-05 12:01:58 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,18 @@
+userId
+password
+
+import mysql.connector
+
+mydb=mysql.connector.connect(
+    host="ocalhost"
+    database="dbs_project"
+)
+mycursor=mydb.cursor()
+
+checkUserId = mycursor.execute("Select userId from registered_users r where r.userId=? and r.password=?",(userId,password))
+
+if checkUserId !=0
+    
+
+
+
